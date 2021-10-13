fb02834d592cac163fe5e3680919cd5a578fed76
brianmwangi <Nairobi@2020>
2021-10-14 00:40:00 +0300

    Added recipe list state and viewmodel

---
+++ app/src/main/java/com/example/eatery/presentation/recipe_list/RecipesListViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.eatery.presentation.recipe_list
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eatery.common.Resource
+import com.example.eatery.domain.use_cases.getRecipes.GetRecipesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipeListViewModel @Inject constructor(
+    private val getRecipesUseCase: GetRecipesUseCase
+) : ViewModel() {
+    private val _state = mutableStateOf(RecipeListState())
+    val state : State<RecipeListState> = _state
+
+    init {
+        getRecipes()
+    }
+
+    private fun getRecipes() {
+        getRecipesUseCase(num = 15).onEach { result ->
+            when(result) {
+                is Resource.Success -> {
+                    _state.value = RecipeListState(recipes = result.data ?: emptyList())
+                }
+                is Resource.Loading -> {
+                    _state.value = RecipeListState(isLoading = true)
+
+                }
+                is Resource.Error -> {
+                    _state.value = RecipeListState(error = result.message ?: "An unexpected error occurred")
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
